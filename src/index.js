1d947d529da6605beb2fa97bbe549f55181b88e5
adewunmi <shonibareadewunmicomfort@gmail.com>
2021-10-12 11:09:10 +0100

    add footer

--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
 import './style.css';
+import Logo from '../asset/images/logo.png';
+
+const myFooter = document.getElementById('footer');
+document.body.appendChild(myFooter);
+const p = document.createElement('P');
+const txt = document.createTextNode('Copyright ©, 2021');
+txt.class = 'flex';
+p.appendChild(txt);
+myFooter.appendChild(p);
+
+const logoImg = document.getElementById('logoDiv');
+const myIcon = new Image();
+myIcon.src = Logo;
+myIcon.alt = 'Pokemon';
+myIcon.id = 'logo';
+logoImg.appendChild(myIcon);
