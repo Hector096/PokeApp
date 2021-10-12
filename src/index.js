d293594dacb0724b2c179312e069da55531e55f4
Adewunmi97 <shonibareadewunmicomfort@gmail.com>
2021-10-12 16:24:50 +0100

    add footer

--- src/index.js
+++ src/index.js
@@ -10,6 +10,17 @@ myIcon.alt = 'Pokemon';
 myIcon.id = 'logo';
 logoImg.appendChild(myIcon);
 
+const myFooter = document.getElementById('footer');
+myFooter.classList.add('d-flex');
+myFooter.classList.add('justify-content-center');
+myFooter.classList.add('align-items-center');
+document.body.appendChild(myFooter);
+const p1 = document.createElement('p');
+const txt1 = document.createTextNode('Copyright © 2021 Created By Ade & Hector');
+p1.className += (' text-muted text-center');
+p1.appendChild(txt1);
+myFooter.appendChild(p1);
+
 const limit = 32;
 const offset = 1;
 
