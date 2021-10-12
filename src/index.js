9e8fc3754858f9bb9264061b0ce73d8661dfe796
Adewunmi97 <shonibareadewunmicomfort@gmail.com>
2021-10-12 13:52:54 +0100

    fix linters error

--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
 import './style.css';
 import Logo from '../asset/images/logo.png';
 
-
-
 const logoImg = document.getElementById('logoDiv');
 const myIcon = new Image();
 myIcon.src = Logo;
@@ -11,9 +9,9 @@ myIcon.id = 'logo';
 logoImg.appendChild(myIcon);
 
 const myFooter = document.getElementById('footer');
-myFooter.classList.add('d-flex')
-myFooter.classList.add('justify-content-center')
-myFooter.classList.add('align-items-center')
+myFooter.classList.add('d-flex');
+myFooter.classList.add('justify-content-center');
+myFooter.classList.add('align-items-center');
 document.body.appendChild(myFooter);
 const p1 = document.createElement('p');
 const txt1 = document.createTextNode('Copyright © 2021 Created By Ade & Hector');
