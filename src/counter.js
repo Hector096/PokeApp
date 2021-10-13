9619f40f93616af1ac69fa97864270dfb1b3eda2
Hector <vv96rca@gmail.com>
2021-10-13 19:00:16 +0530

    resolved linting issues

--- src/counter.js
+++ src/counter.js
@@ -1,6 +1,6 @@
 const counter = {
-    pokemon: (allPokemon) => allPokemon.length,
-    comments: (allComments) => allComments.length
-  };
-  
-  export default counter;
+  pokemon: (allPokemon) => allPokemon.length,
+  comments: (allComments) => allComments.length,
+};
+
+export default counter;
--- src/data.js
+++ src/data.js
@@ -1,36 +1,35 @@
-const BASE_URL =
-  "https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/vmeYO55GvyryU3TCtGRE";
+const BASE_URL = 'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/vmeYO55GvyryU3TCtGRE';
 
 export const getPokemon = async (id) => {
   try {
     const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`)
       .then((response) => response.json())
       .then((json) => json);
-    return response
+    return response;
   } catch (e) {
     throw new Error(`API error! status: ${e.toString()}`);
   }
 };
 
 export const getLikes = async () => {
-    const response = await fetch(`${BASE_URL}/likes/`);
-    if (!response.ok) {
-      throw new Error(`API error! status: ${response.status}`);
-    } else {
-      const data = await response.json();
-      localStorage.setItem('likes',JSON.stringify(data))
-      return data;
-    }
+  const response = await fetch(`${BASE_URL}/likes/`);
+  if (!response.ok) {
+    throw new Error(`API error! status: ${response.status}`);
+  } else {
+    const data = await response.json();
+    localStorage.setItem('likes', JSON.stringify(data));
+    return data;
+  }
 };
 export const postLike = async (id) => {
   try {
     const response = await fetch(`${BASE_URL}/likes/`, {
-      method: "POST",
+      method: 'POST',
       body: JSON.stringify({
         item_id: id,
       }),
       headers: {
-        "Content-type": "application/json; charset=UTF-8",
+        'Content-type': 'application/json; charset=UTF-8',
       },
     })
       .then((response) => response.text())
@@ -55,14 +54,14 @@ export const getComment = async (id) => {
 export const postComment = async (itemId, username, comment) => {
   try {
     const response = await fetch(`${BASE_URL}/comments/`, {
-      method: "POST",
+      method: 'POST',
       body: JSON.stringify({
         item_id: itemId,
         username,
         comment,
       }),
       headers: {
-        "Content-type": "application/json; charset=UTF-8",
+        'Content-type': 'application/json; charset=UTF-8',
       },
     })
       .then((response) => response.json())
